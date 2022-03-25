211784758d172c3bf339faa6159a70c5d774b502
Rusben <rusben@gmail.com>
2022-03-25 09:30:07 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+fetch("./header.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("header").innerHTML = data;
+  });
+
+fetch("./menu.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("menu").innerHTML = data;
+  });
+
+fetch("./footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("footer").innerHTML = data;
+  });
